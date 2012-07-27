50abc142f20ee2ba8b2c3007b4ffb038950f096d
tonyo <tonyo@rightclickblog.com>
2012-07-27 14:33:30 -0700

    fixed a bug

--- sklog.js
+++ sklog.js
@@ -5,12 +5,12 @@ var loglevels = [
    ,"[" + c.red.bold("ERROR") + "]: "
 ];
 
-var shallow = function(o){var d="\n";for(var k in o){d+="\t"+c.green(k)+c.white("\t:\t")+c.black(m[k]+"").replace("\n","").substring(0,40)+"\n";}return d;};
+var shallow = function(o){var d="\n";for(var k in o){d+="\t"+c.red(k)+c.black("\t:\t")+c.black(o[k]+"").replace("\n","").substring(0,40)+"\n";}return d;};
 
 function log(){
    var level = (isNaN(arguments[0]) || arguments[0] >= loglevels.length)?0:arguments[0];
    var msg;
-   for(var i=1;i<arguments.length;i++){
+   for(var i=isNaN(arguments[0]) ? 0 : 1;i<arguments.length;i++){
       msg = "";
       try{
          if(typeof arguments[i] == "object"){
